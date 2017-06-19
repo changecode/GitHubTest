617f13edc39d0cd5e8069f4ad9272bd8da81cf57
Tim <c779333@163.com>
2017-06-19 18:01:26 +0800

    修改目录、一些错误提示信息

--- javautils/src/main/java/com/java/utils/excel/exportExcelPicture.java
+++ javautils/src/main/java/com/java/utils/excel/exportExcelPicture.java
@@ -1,3 +1,18 @@
+package com.java.utils.excel;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.poi.hssf.usermodel.HSSFClientAnchor;
+import org.apache.poi.hssf.usermodel.HSSFPatriarch;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 public class exportExcelPicture{
     
     public static void main(String[] args) {
