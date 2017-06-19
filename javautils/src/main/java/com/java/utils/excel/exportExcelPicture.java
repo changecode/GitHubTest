bf81c7cdddf8f7f33cc3d58f3483600925fe7537
Tim <c779333@163.com>
2017-06-19 15:37:46 +0800

    插入Excel图片导出

---
+++ javautils/src/main/java/com/java/utils/excel/exportExcelPicture.java
@@ -0,0 +1,39 @@
+public class exportExcelPicture{
+    
+    public static void main(String[] args) {
+         FileOutputStream fileOut = null;   
+         BufferedImage bufferImg = null;   
+        //先把读进来的图片放到一个ByteArrayOutputStream中，以便产生ByteArray  
+        try {
+            ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();   
+            bufferImg = ImageIO.read(new File("F:/图片/照片/无名氏/小昭11.jpg"));   
+            ImageIO.write(bufferImg, "jpg", byteArrayOut);
+            
+            HSSFWorkbook wb = new HSSFWorkbook();   
+            HSSFSheet sheet1 = wb.createSheet("test picture");  
+            //画图的顶级管理器，一个sheet只能获取一个（一定要注意这点）
+            HSSFPatriarch patriarch = sheet1.createDrawingPatriarch();   
+            //anchor主要用于设置图片的属性
+            HSSFClientAnchor anchor = new HSSFClientAnchor(0, 0, 255, 255,(short) 1, 1, (short) 5, 8);   
+            anchor.setAnchorType(3);   
+            //插入图片  
+            patriarch.createPicture(anchor, wb.addPicture(byteArrayOut.toByteArray(), HSSFWorkbook.PICTURE_TYPE_JPEG)); 
+            fileOut = new FileOutputStream("D:/测试Excel.xls");   
+            // 写入excel文件   
+             wb.write(fileOut);   
+             System.out.println("----Excle文件已生成------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(fileOut != null){
+                 try {
+                    fileOut.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+}
+
