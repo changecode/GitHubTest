3da10c024750e143aac710eee5184213a12b030f
Tim <c779333@163.com>
2017-06-20 17:38:29 +0800

    mybatis生成工具

---
+++ mybatis-generator/src/main/java/com/gerator/entity/user.java
@@ -0,0 +1,405 @@
+package com.gerator.entity;
+
+import java.util.Date;
+
+public class user {
+    private Long id;
+
+    private Date createTime;
+
+    private Long createBy;
+
+    private Date lastUpdateTime;
+
+    private Long lastUpdateBy;
+
+    private Boolean enable;
+
+    private String userName;
+
+    private String nick;
+
+    private String password;
+
+    private String mobile;
+
+    private String email;
+
+    private String gender;
+
+    private Long enterpriseId;
+
+    private String salt;
+
+    private String userCode;
+
+    private String status;
+
+    private String tel;
+
+    private Long jobId;
+
+    private String userType;
+
+    private Long departmentId;
+
+    private String attribute1;
+
+    private String attribute2;
+
+    private String attribute3;
+
+    private String attribute4;
+
+    private String attribute5;
+
+    private String attribute6;
+
+    private String attribute7;
+
+    private String attribute8;
+
+    private String attribute9;
+
+    private String attribute10;
+
+    private String attribute11;
+
+    private String attribute12;
+
+    private String attribute13;
+
+    private String attribute14;
+
+    private String attribute15;
+
+    private String attribute16;
+
+    private String attribute17;
+
+    private String attribute18;
+
+    private String attribute19;
+
+    private String attribute20;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Long createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public Long getLastUpdateBy() {
+        return lastUpdateBy;
+    }
+
+    public void setLastUpdateBy(Long lastUpdateBy) {
+        this.lastUpdateBy = lastUpdateBy;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Long getEnterpriseId() {
+        return enterpriseId;
+    }
+
+    public void setEnterpriseId(Long enterpriseId) {
+        this.enterpriseId = enterpriseId;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public Long getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Long jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getAttribute1() {
+        return attribute1;
+    }
+
+    public void setAttribute1(String attribute1) {
+        this.attribute1 = attribute1;
+    }
+
+    public String getAttribute2() {
+        return attribute2;
+    }
+
+    public void setAttribute2(String attribute2) {
+        this.attribute2 = attribute2;
+    }
+
+    public String getAttribute3() {
+        return attribute3;
+    }
+
+    public void setAttribute3(String attribute3) {
+        this.attribute3 = attribute3;
+    }
+
+    public String getAttribute4() {
+        return attribute4;
+    }
+
+    public void setAttribute4(String attribute4) {
+        this.attribute4 = attribute4;
+    }
+
+    public String getAttribute5() {
+        return attribute5;
+    }
+
+    public void setAttribute5(String attribute5) {
+        this.attribute5 = attribute5;
+    }
+
+    public String getAttribute6() {
+        return attribute6;
+    }
+
+    public void setAttribute6(String attribute6) {
+        this.attribute6 = attribute6;
+    }
+
+    public String getAttribute7() {
+        return attribute7;
+    }
+
+    public void setAttribute7(String attribute7) {
+        this.attribute7 = attribute7;
+    }
+
+    public String getAttribute8() {
+        return attribute8;
+    }
+
+    public void setAttribute8(String attribute8) {
+        this.attribute8 = attribute8;
+    }
+
+    public String getAttribute9() {
+        return attribute9;
+    }
+
+    public void setAttribute9(String attribute9) {
+        this.attribute9 = attribute9;
+    }
+
+    public String getAttribute10() {
+        return attribute10;
+    }
+
+    public void setAttribute10(String attribute10) {
+        this.attribute10 = attribute10;
+    }
+
+    public String getAttribute11() {
+        return attribute11;
+    }
+
+    public void setAttribute11(String attribute11) {
+        this.attribute11 = attribute11;
+    }
+
+    public String getAttribute12() {
+        return attribute12;
+    }
+
+    public void setAttribute12(String attribute12) {
+        this.attribute12 = attribute12;
+    }
+
+    public String getAttribute13() {
+        return attribute13;
+    }
+
+    public void setAttribute13(String attribute13) {
+        this.attribute13 = attribute13;
+    }
+
+    public String getAttribute14() {
+        return attribute14;
+    }
+
+    public void setAttribute14(String attribute14) {
+        this.attribute14 = attribute14;
+    }
+
+    public String getAttribute15() {
+        return attribute15;
+    }
+
+    public void setAttribute15(String attribute15) {
+        this.attribute15 = attribute15;
+    }
+
+    public String getAttribute16() {
+        return attribute16;
+    }
+
+    public void setAttribute16(String attribute16) {
+        this.attribute16 = attribute16;
+    }
+
+    public String getAttribute17() {
+        return attribute17;
+    }
+
+    public void setAttribute17(String attribute17) {
+        this.attribute17 = attribute17;
+    }
+
+    public String getAttribute18() {
+        return attribute18;
+    }
+
+    public void setAttribute18(String attribute18) {
+        this.attribute18 = attribute18;
+    }
+
+    public String getAttribute19() {
+        return attribute19;
+    }
+
+    public void setAttribute19(String attribute19) {
+        this.attribute19 = attribute19;
+    }
+
+    public String getAttribute20() {
+        return attribute20;
+    }
+
+    public void setAttribute20(String attribute20) {
+        this.attribute20 = attribute20;
+    }
+}
---
+++ mybatis-generator/src/main/java/org/gerator/dao/userMapper.java
@@ -0,0 +1,17 @@
+package org.gerator.dao;
+
+import com.gerator.entity.user;
+
+public interface userMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(user record);
+
+    int insertSelective(user record);
+
+    user selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(user record);
+
+    int updateByPrimaryKey(user record);
+}
